78ae6693d1e96dc36371446bc8f1b6c957f83a72
Trisse6969 <tristan.covemaeker1@gmail.com>
2022-02-25 15:51:46 +0100

    added product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Product } from '../models/product';
+
+const mock_products = [
+  {id: 1, name: 'Adidas Stan Smith', price: 90.0, category: 'Shoes', description: ''},
+  {id: 2, name: 'Nike Air Max', price: 110.0, category: 'Shoes', description: ''},
+  {id: 3, name: 'Reebok Sweat Shirt', price: 45.0, category: 'Clothes', description: ''},
+  {id: 4, name: 'Puma T-Shirt', price: 30.0, category: 'Clothes', description: ''}
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  getProducts(): Observable<Product[]> {
+    return of(mock_products);
+  }
+
+  getProduct(id: number): Observable<Product> {
+    return of(mock_products[id-1]);
+  }
+
+  constructor() { }
+}
