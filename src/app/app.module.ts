eb14a514212c5b4141d6147b8258adfe75c6d811
Trisse6969 <tristan.covemaeker1@gmail.com>
2022-02-25 15:53:22 +0100

    misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './components/admin/products/products.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductDetailComponent } from './components/admin/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
