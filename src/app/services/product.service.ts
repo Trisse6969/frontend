bc97c89679be52f2fcc384e93d03104b80da61c4
Trisse6969 <tristan.covemaeker1@gmail.com>
2022-03-01 17:44:32 +0100

    changed mock data to server data

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { Product } from '../models/product';
-
-const mock_products = [
-  {id: 1, name: 'Adidas Stan Smith', price: 90.0, category: 'Shoes', description: ''},
-  {id: 2, name: 'Nike Air Max', price: 110.0, category: 'Shoes', description: ''},
-  {id: 3, name: 'Reebok Sweat Shirt', price: 45.0, category: 'Clothes', description: ''},
-  {id: 4, name: 'Puma T-Shirt', price: 30.0, category: 'Clothes', description: ''}
-];
+import { ProductPayload } from '../models/productPayload';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getProducts(): Observable<Product[]> {
-    return of(mock_products);
+
+  productsUrl = `${environment.apiUrl}/products`;
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  getProducts(): Observable<ProductPayload> {
+    return this.http.get<ProductPayload>(this.productsUrl)
+      .pipe(catchError(this.handleError<ProductPayload>('getProducts', { products: [], count: 0})))
   }
 
   getProduct(id: number): Observable<Product> {
-    return of(mock_products[id-1]);
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(url)
+      .pipe(catchError(this.handleError<Product>(`getProduct/${id}`)));
   }
 
-  constructor() { }
+  saveProduct(product: Product): Observable<Product> {
+    const url = `${this.productsUrl}/${product.id}`
+    return this.http.put<Product>(url, product, this.httpOptions)
+      .pipe(
+        catchError(this.handleError('saveProduct', product))
+      );
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    const url = `${this.productsUrl}/${id}`
+    return this.http.delete<Product>(url)
+      .pipe(
+        catchError(this.handleError<Product>('deleteProduct'))
+      );
+  }
+
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.productsUrl, product, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<Product>('addProduct', product))
+      );
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any) : Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    }
+  }
+
+  constructor(private http: HttpClient) { }
 }
