aec4b0a597a102516c3ca3338d3044fbf2161ae6
Trisse6969 <tristan.covemaeker1@gmail.com>
2022-03-01 17:43:57 +0100

    added addProduct route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from './components/admin/products/products.component';
 import { ProductDetailComponent } from './components/admin/product-detail/product-detail.component';
+import { ProductAddComponent } from './components/admin/product-add/product-add.component';
 
 const routes: Routes = [
   {path: '', component: ProductsComponent},
   {path: 'products', component: ProductsComponent},
+  {path: 'products/add', component: ProductAddComponent},
   {path: 'products/:id', component: ProductDetailComponent}
 ]
 
