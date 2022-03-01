27c0ff31d512725c85925737d7e649b2699ac6c2
Trisse6969 <tristan.covemaeker1@gmail.com>
2022-03-01 17:47:44 +0100

    added libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsComponent } from './components/admin/products/products.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductDetailComponent } from './components/admin/product-detail/product-detail.component';
+import { ProductAddComponent } from './components/admin/product-add/product-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
